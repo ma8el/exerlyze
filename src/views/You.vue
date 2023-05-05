2a5dac0a7971c1fb06143cfcd9be14221dcce52d
Markus Kurbel <m4rku@pm.me>
2023-05-06 00:01:53 +0200

    Reformat pages

--- src/views/You.vue
+++ src/views/You.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
+
 <template>
   <ion-page>
     <ion-header>
@@ -16,8 +21,3 @@
     </ion-content>
   </ion-page>
 </template>
-
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-</script>
