84b79a842e3722d873cb41badb12824375e4b72c
Markus Kurbel <m4rku@pm.me>
2023-05-06 15:39:04 +0200

    Add basic cypress tests

--- tests/e2e/specs/test.cy.ts
+++ tests/e2e/specs/test.cy.ts
@@ -1,6 +1,26 @@
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('ion-content', 'Tab 1 page')
+describe('Page test', () => {
+  it('Visits home page', () => {
+    cy.visit('/tabs/home')
+    cy.contains('ion-content', 'Home page')
+  })
+
+  it('Visits workouts page', () => {
+    cy.visit('/tabs/workouts')
+    cy.contains('ion-content', 'Workouts page')
+  })
+
+  it('Visits track page', () => {
+    cy.visit('/tabs/track')
+    cy.contains('ion-content', 'Track page')
+  })
+
+  it('Visits nutrition page', () => {
+    cy.visit('/tabs/nutrition')
+    cy.contains('ion-content', 'Nutrition page')
+  })
+
+  it('Visits you page', () => {
+    cy.visit('/tabs/you')
+    cy.contains('ion-content', 'You page')
   })
 })
