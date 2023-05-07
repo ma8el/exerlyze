b92a16e223981d2ff75970d22abc9f205dc30027
Markus Kurbel <m4rku@pm.me>
2023-05-07 23:19:56 +0200

    Add workout plans card

---
+++ src/components/WorkoutPlanCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/vue';
+  import WorkoutPlanItem from './WorkoutPlanItem.vue';
+
+  const workouts = [
+    {
+      workoutName: 'Workout 1',
+      plannedDay: 'Monday'
+    },
+   {
+      workoutName: 'Workout 2',
+      plannedDay: 'Wednesday'
+    },
+   {
+      workoutName: 'Workout 3',
+      plannedDay: 'Saturday'
+    },
+ ]
+
+</script>
+
+<template>
+  <ion-card>
+    <ion-card-header>
+    <ion-card-title>Workout Plan Name</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <WorkoutPlanItem 
+        v-for = "workout in workouts"
+        :workoutName="workout.workoutName"
+        :plannedDay="workout.plannedDay"
+      />
+    </ion-card-content>
+  </ion-card>
+</template>
---
+++ src/components/WorkoutPlanItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { IonItem, IonLabel, IonNote } from '@ionic/vue';
+  const props = defineProps({
+      workoutName: String,
+      plannedDay: String
+  })
+</script>
+
+<template>
+ <ion-item>
+    <ion-label>{{ workoutName }}</ion-label>
+    <ion-note slot="end">{{ plannedDay }}</ion-note>
+  </ion-item>
+</template>
