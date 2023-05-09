736d3d9825dd90460f132261899f9f8178678d84
Markus Kurbel <m4rku@pm.me>
2023-05-09 15:25:29 +0200

    Add track modal

---
+++ src/components/Modals/TrackModal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { IonButton, IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, modalController } from '@ionic/vue';
+
+const closeModal = () => {
+    return modalController.dismiss(null, 'close');
+};
+</script>
+
+<template>
+  <ion-content>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Track</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="closeModal">Dismiss</ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+    </ion-content>
+  </ion-content>
+</template>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,8 +5,26 @@
            IonLabel,
            IonIcon,
            IonPage,
-           IonRouterOutlet} from '@ionic/vue';
-  import { personCircleOutline, nutritionOutline, barbellOutline, homeSharp, addCircleOutline } from 'ionicons/icons';
+           IonRouterOutlet,
+           modalController} from '@ionic/vue';
+  import { personCircleOutline, nutritionOutline, barbellOutline, homeSharp, add } from 'ionicons/icons';
+  import TrackModal from '@/components/Modals/TrackModal.vue';
+  
+  const openModal = async () => {
+    const modal = await modalController.create({
+      component: TrackModal,
+      cssClass: 'full-screen-modal',
+      initialBreakpoint: 0.5,
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+    if (role == 'save') {
+      console.log('Save', data);
+    } else {
+      console.log('Close', data);
+    };
+  }
 
 </script>
 
@@ -14,7 +32,7 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar slot="bottom" class="custom-tab-bar">
         <ion-tab-button tab="home" href="/tabs/home">
           <ion-icon aria-hidden="true" :icon="homeSharp" />
           <ion-label>Home</ion-label>
@@ -25,9 +43,8 @@
           <ion-label>Workouts</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="track" href="/tabs/track">
-          <ion-icon aria-hidden="true" :icon="addCircleOutline" />
-          <ion-label>Track</ion-label>
+        <ion-tab-button @click="openModal" class="floating-action-button">
+          <ion-icon  :icon="add" />
         </ion-tab-button>
 
         <ion-tab-button tab="nutrition" href="/tabs/nutrition">
@@ -43,3 +60,20 @@
     </ion-tabs>
   </ion-page>
 </template>
+
+<style scoped lang="scss">
+.floating-action-button {
+  height: 50px;
+  border-radius: 50%;
+  background-color: #3F63C8;
+
+  ion-icon {
+    font-size: 24px;
+    color: #ffffff;
+  }
+}
+.custom-tab-bar {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
