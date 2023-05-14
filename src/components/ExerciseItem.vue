925718acb0a8e79dab4e64d8228c73ff8db4a58f
Markus Kurbel <m4rku@pm.me>
2023-05-14 17:26:53 +0200

    Make exercises modifiable

--- src/components/ExerciseItem.vue
+++ src/components/ExerciseItem.vue
@@ -1,16 +1,63 @@
 <script setup lang="ts">
-  import { IonItem, IonLabel, IonNote } from '@ionic/vue';
+  import { IonInput, IonItem, IonLabel, IonThumbnail } from '@ionic/vue';
+  import { ref, onMounted } from 'vue'
+
+  const name = ref<string>()
+  const sets = ref<number>()
+  const reps = ref<number>()
+  const weight = ref<number>()
+
   const props = defineProps({
       exerciseName: String,
       exerciseSets: Number,
       exerciseReps: Number,
       exerciseWeight: Number
   })
+
+  onMounted (() => {
+    name.value = props.exerciseName
+    sets.value = props.exerciseSets
+    reps.value = props.exerciseReps
+    weight.value = props.exerciseWeight
+  })
 </script>
 
 <template>
   <ion-item>
-    <ion-label>{{ exerciseName }}</ion-label>
-    <ion-note slot="end">{{ exerciseSets }} sets x {{ exerciseReps }} reps x {{ exerciseWeight }} kg</ion-note>
+    <ion-thumbnail slot="start">
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+    </ion-thumbnail>
+    <ion-label>
+      <h3>{{ exerciseName }}</h3>
+      <div class="single-line">
+        <ion-input
+          label="Sets"
+          label-placement="stacked"
+          v-model="sets"
+          fill="solid"
+        >
+        </ion-input>
+        <ion-input
+          label="Reps"
+          label-placement="stacked"
+          v-model="reps"
+          fill="solid"
+        >
+        </ion-input>
+        <ion-input
+          label="Weight"
+          label-placement="stacked"
+          v-model="weight"
+          fill="solid"
+        >
+        </ion-input>
+      </div>
+    </ion-label>
   </ion-item>
 </template>
+
+<style scoped>
+.single-line {
+  display: inline-flex;
+}
+</style>
