2aaad032223aae2b86109f2356206f8fd8aaddc6
Markus Kurbel <m4rku@pm.me>
2023-05-21 14:57:27 +0200

    Refactor WorkoutCard

--- src/components/Cards/WorkoutCard.vue
+++ src/components/Cards/WorkoutCard.vue
@@ -3,67 +3,29 @@
            IonCardHeader,
            IonCardTitle,
            IonCardContent,
-           IonModal,
-           IonHeader,
-           IonToolbar,
-           IonButton,
-           IonButtons,
-           IonTitle,
-           IonContent,
            modalController } from '@ionic/vue';
-  import ExerciseItem from '@/components/ExerciseItem.vue';
-  import { ref } from 'vue';
   import { Workout } from '@/types';
+  import AddWorkoutModal from '../Modals/AddWorkoutModal.vue';
 
   const props = defineProps<Workout>()
 
-  const modal = ref(null)
-
-  const onWillDismiss = () => {
-    console.log('will dismiss')
+  const openModal = async () => {
+    const modal = await modalController.create({
+      component: AddWorkoutModal,
+      componentProps: { workoutId: props.id },
+      cssClass: 'full-screen-modal',
+    });
+    modal.present();
   }
-
-  const cancel = () => {
-    modalController.dismiss(null, 'cancel')
-  }
-
-  const confirm = () => {
-    modalController.dismiss(null, 'confirm')
-  }
-
 </script>
 
 <template>
-  <ion-card :button="true" :id="`open-modal-${id}`">
+  <ion-card :button="true" @click="openModal">
     <ion-card-header>
       <ion-card-title>{{ name }}</ion-card-title>
     </ion-card-header>
     <ion-card-content>
       Meta data
     </ion-card-content>
-    <ion-modal ref="modal" :trigger="`open-modal-${id}`" @willDismiss="onWillDismiss">
-        <ion-header>
-          <ion-toolbar>
-            <ion-buttons slot="start">
-              <ion-button @click="cancel()">Cancel</ion-button>
-            </ion-buttons>
-            <ion-title>{{ name }}</ion-title>
-            <ion-buttons slot="end">
-              <ion-button :strong="true" @click="confirm()">Confirm</ion-button>
-            </ion-buttons>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding">
-          <ExerciseItem 
-            v-for = "exercise in exercises"
-            :key="exercise.id"
-            :id="exercise.id"
-            :name="exercise.name"
-            :sets="exercise.sets"
-            :reps="exercise.reps"
-            :weight="exercise.weight"
-          />
-        </ion-content>
-      </ion-modal>
-    </ion-card>
+  </ion-card>
 </template>
--- src/components/Modals/AddWorkoutModal.vue
+++ src/components/Modals/AddWorkoutModal.vue
@@ -2,19 +2,25 @@
   import { IonButton,
            IonInput,
            modalController } from '@ionic/vue';
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
   import { useWorkoutStore } from '../../store/workoutStore';
   import AddExerciseButton from '../Buttons/AddExerciseButton.vue'
   import ExerciseItem from '../ExerciseItem.vue';
-  import { Exercise } from '@/types';
   import BaseFullPageModal from './BaseFullPageModal.vue';
+  import { Exercise } from '@/types';
+
+  const props = defineProps({
+    workoutId: {
+      type: Number,
+    }
+  })
 
   const workoutStore = useWorkoutStore()
-
+  
   const workoutName = ref()
   const description = ref()
   const exercises = ref<Exercise[]>([])
-  
+
   const save = () => {
     console.log(exercises.value)
     workoutStore.addWorkout({
@@ -26,15 +32,37 @@
     modalController.dismiss(null, 'save');
   }
 
+  const update = () => {
+    if (!props.workoutId) return
+    workoutStore.updateWorkout({
+      id: props.workoutId,
+      name: workoutName.value,
+      description: description.value,
+      exercises: exercises.value
+    })
+    modalController.dismiss(null, 'save');
+  }
+
   const addExercise = (e: Exercise[]) => {
     exercises.value.push(...e)
   }
+
+  onMounted(() => {
+    if (props.workoutId) {
+      const workout = workoutStore.getWorkoutById(props.workoutId)
+      if (!workout) return
+      workoutName.value = workout.name
+      description.value = workout.description
+      exercises.value = workout.exercises
+    }
+  })
 </script>
 
  <template>
   <BaseFullPageModal title="Add Workout">
     <template #saveButton>
-      <ion-button @click="save">Save</ion-button>
+      <ion-button v-if="!workoutId" @click="save">Save</ion-button>
+      <ion-button v-else @click="update">Update</ion-button>
     </template>
     <template #modalContent>
       <ion-input class="ion-padding" fill="outline" shape="round" label="Workout Name" label-placement="stacked" v-model="workoutName"></ion-input>
