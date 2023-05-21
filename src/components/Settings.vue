95796a07c0d544cfe321f7414af6b385b9a0e4d2
Markus Kurbel <m4rku@pm.me>
2023-05-21 14:37:05 +0200

    Refactor SettingsModal

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,33 +1,22 @@
 <script setup lang="ts">
-import { IonButton, IonContent, IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonToggle, IonButtons, modalController } from '@ionic/vue';
-import { ref } from 'vue';
+  import { IonButton, IonList, IonItem, IonToggle, modalController } from '@ionic/vue';
+  import { ref } from 'vue';
+  import BaseFullPageModal from './Modals/BaseFullPageModal.vue';
+  
+  const setting1 = ref(false);
+  const setting2 = ref(false);
 
-const setting1 = ref(false);
-const setting2 = ref(false);
-
-const save = () => {
-    return modalController.dismiss({ setting1: setting1.value, setting2: setting2.value }, 'save');
-};
-
-const closeModal = () => {
-    return modalController.dismiss(null, 'close');
-};
+  const save = () => {
+      return modalController.dismiss({ setting1: setting1.value, setting2: setting2.value }, 'save');
+  };
 </script>
 
 <template>
-  <ion-content>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Settings</ion-title>
-        <ion-buttons slot="end">
+  <BaseFullPageModal title="Settings">
+    <template #saveButton>
           <ion-button @click="save">Save</ion-button>
-        </ion-buttons>
-        <ion-buttons slot="end">
-          <ion-button @click="closeModal">Close</ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
+    </template>
+    <template #modalContent>
       <ion-list>
         <ion-item>
           <ion-toggle v-model="setting1">Setting 1</ion-toggle>
@@ -36,6 +25,6 @@ const closeModal = () => {
           <ion-toggle v-model="setting2">Setting 2</ion-toggle>
         </ion-item>
       </ion-list>
-    </ion-content>
-  </ion-content>
+    </template>
+  </BaseFullPageModal>
 </template>
