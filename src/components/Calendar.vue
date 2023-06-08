66bc9b6cf97f320364ec19e751b11326dee428e3
Markus Kurbel <m4rku@pm.me>
2023-06-08 14:43:35 +0200

    Add workout calendar highlights

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,14 +1,53 @@
 <script setup lang="ts">
   import { IonGrid, IonRow } from '@ionic/vue';
   import { useDark } from '@vueuse/core'
+  import { ref, PropType } from 'vue'
 
+  const props = defineProps({
+    workoutEventDates: {
+      type: Array as PropType<Date[]>,
+      required: false,
+      default: () => []
+    },
+    nutitionDiaryEventDates: {
+      type: Array as PropType<Date[]>,
+      required: false,
+      default: () => []
+    },
+  })
   const isDark = useDark()
+
+  const attributes = ref([{
+      key: 'today',
+      highlight: {
+        color: '#3F63C8',
+        fillMode: 'solid',
+      },
+      dates: new Date(),
+  },
+  {
+      key: 'workoutEvent',
+      dot: {
+        style: {
+          backgroundColor: '#3F63C8',
+        },
+      },
+      dates: props.workoutEventDates,
+  }
+  ])
 </script>
 
 <template>
   <ion-grid>
     <ion-row class="ion-justify-content-center">
-        <VCalendar expanded transparent borderless :is-dark="isDark" view="weekly" />
+        <VCalendar 
+          expanded 
+          transparent 
+          borderless 
+          :is-dark="isDark" 
+          view="weekly"
+          :attributes="attributes"
+        />
     </ion-row>
   </ion-grid>
 </template>
--- src/layouts/PageWithCalendar.vue
+++ src/layouts/PageWithCalendar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import AppLayout from '@/layouts/AppLayout.vue';
   import Calendar from '@/components/Calendar.vue';
+  import { useWorkoutPlanStore } from '@/store/workoutStore';
 
   const props = defineProps({
     title: {
@@ -8,11 +9,36 @@
       required: true
     }
   });
+
+  const workoutPlanStore = useWorkoutPlanStore();
+
+  const getCurrentWeekDates = () => {
+    var now = new Date();
+    var dayOfWeek = now.getDay();
+    var numDay = dayOfWeek !== 0 ? dayOfWeek - 1 : 6;
+
+    now.setDate(now.getDate() - numDay);
+
+    var dates = [];
+
+    for(var i = 0; i < 7; i++) {
+        dates.push(new Date(now));
+        now.setDate(now.getDate() + 1);
+    }
+
+    return dates;
+  }
+
+  const plannedWorkouts = workoutPlanStore.getFullWorkoutPlans;
+  const workoutEventDates = getCurrentWeekDates().filter(
+    date => plannedWorkouts.some(workout => workout.dayOfWeekId === date.getDay()))
 </script>
 
 <template>
   <AppLayout :title="title">
-    <Calendar />
+    <Calendar
+      :workout-event-dates="workoutEventDates"
+    />
     <slot></slot>
   </AppLayout>
 </template>
