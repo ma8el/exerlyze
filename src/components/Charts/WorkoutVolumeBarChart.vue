ae341f756cadf6f226505fa3653a7a37e998ac05
Markus Kurbel <m4rku@pm.me>
2023-06-13 21:40:02 +0200

    Add dummy barchart

---
+++ src/components/Charts/WorkoutVolumeBarChart.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/vue'
+import {
+  Chart as ChartJS,
+  Title,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import { useDayOfWeekStore } from '@/store/workoutStore';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Legend)
+
+const dayOfWeekStore = useDayOfWeekStore();
+
+const data = {
+  labels: dayOfWeekStore.daysOfWeek.map(d => d.name),
+  datasets: [
+    {
+      label: 'Workout Volume',
+      backgroundColor: '#3F63C8',
+      data: [40, 20, 12, 39, 10, 40, 39]
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: true
+}
+</script>
+
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>Workout Volume</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <Bar :data="data" :options="options" />
+    </ion-card-content>
+  </ion-card>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import UpcomingEventsSlide from '@/components/UpcomingEventsSlide.vue';
   import PageWithCalendar from '@/layouts/PageWithCalendar.vue';
+  import WorkoutVolumeBarChart from '@/components/Charts/WorkoutVolumeBarChart.vue';
 </script>
 
 <template>
   <PageWithCalendar title="Home">
     <UpcomingEventsSlide />
+    <WorkoutVolumeBarChart />
   </PageWithCalendar>
 </template>
