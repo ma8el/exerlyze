5c8467db537b66f6b8061514ee798c8c875514ed
Markus Kurbel <m4rku@pm.me>
2023-09-02 14:05:48 +0200

    Add highlighted tab button

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,9 +5,11 @@
            IonLabel,
            IonIcon,
            IonPage,
+           IonFab,
+           IonFabButton,
            IonRouterOutlet,
            modalController} from '@ionic/vue';
-  import { personCircleOutline, nutritionOutline, barbellOutline, homeSharp, add } from 'ionicons/icons';
+  import { personCircleOutline, nutritionOutline, barbellOutline, homeSharp, addOutline } from 'ionicons/icons';
   import TrackModal from '@/components/Modals/TrackModal.vue';
   
   const openModal = async () => {
@@ -43,9 +45,9 @@
           <ion-label>{{ $t('workouts.title') }}</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button @click="openModal" class="floating-action-button">
-          <ion-icon  :icon="add" />
-        </ion-tab-button>
+        <svg height="50" viewBox="0 0 100 50" width="100" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 0v50H0V0c.543 27.153 22.72 49 50 49S99.457 27.153 99.99 0h.01z" fill="red" fill-rule="evenodd"></path>
+        </svg>
 
         <ion-tab-button tab="nutrition" href="/tabs/nutrition">
           <ion-icon aria-hidden="true" :icon="nutritionOutline" />
@@ -56,24 +58,59 @@
           <ion-icon aria-hidden="true" :icon="personCircleOutline" />
           <ion-label>{{ $t('you.title') }}</ion-label>
         </ion-tab-button>
-      </ion-tab-bar>
+
+        <ion-fab vertical="bottom" horizontal="center" translucent="true">
+          <ion-fab-button @click="openModal">
+            <ion-icon :icon="addOutline"/>
+          </ion-fab-button>
+        </ion-fab>
+     </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
 
 <style scoped lang="scss">
-.floating-action-button {
-  height: 50px;
-  border-radius: 50%;
-  background-color: #3F63C8;
-
-  ion-icon {
-    font-size: 24px;
-    color: #ffffff;
-  }
-}
-.custom-tab-bar {
-  display: flex;
-  justify-content: space-evenly;
+ion-tabs{
+	ion-fab {
+		margin-bottom: env(safe-area-inset-bottom); /* fix notch ios*/
+		ion-fab-button {
+			--box-shadow: none;
+      z-index: -1;
+		}
+	}
+	ion-tab-bar {
+		--border: 0;
+		--background: transparent;
+		position: absolute;
+		bottom: 0;
+		left:0; right: 0;
+		width: 100%;
+		&:after{
+			content: " ";
+			width: 100%;
+			bottom: 0;
+			background: var(--ion-color-light);
+			height: env(safe-area-inset-bottom);
+			position: absolute;
+		}
+		ion-tab-button {
+			--background: var(--ion-color-light);
+		}
+		ion-tab-button.comments {
+			margin-right: 0px;
+			border-top-right-radius: 18px;
+		}
+		ion-tab-button.notifs {
+			margin-left: 0px;
+			border-top-left-radius: 18px;
+		}
+		svg {    
+			width: 72px;
+			margin-top: 19px;
+			path{
+				fill:  var(--ion-color-light);
+			}		
+		}
+	}
 }
 </style>
