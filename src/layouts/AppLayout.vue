2f871b3d0a5dada5646451dc9ef5e8f850e5274f
Markus Kurbel <m4rku@pm.me>
2023-09-03 14:17:00 +0200

    Adjust toolbar

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,25 +1,64 @@
 <script setup lang="ts">
-    import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-    import Toolbar from '@/components/Toolbar.vue';
+  import { IonHeader, IonPage, IonContent, IonToolbar, IonTitle, IonButtons, IonButton, IonIcon, modalController } from '@ionic/vue';
+  import { settingsSharp } from 'ionicons/icons';
+  import Settings from '@/components/Settings.vue';
 
-    const props = defineProps({
+  const props = defineProps({
       title: {
         type: String,
         required: true
       }
+  });
+
+  const openModal = async () => {
+    const modal = await modalController.create({
+      component: Settings,
+      cssClass: 'full-screen-modal',
+      componentProps: {
+        setting1: true,
+        setting2: false
+      }
     });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+    if (role == 'save') {
+      console.log('Save', data);
+    } else {
+      console.log('Close', data);
+    };
+  }
 </script>
 
 <template>
   <ion-page>
-    <Toolbar :title="title" />
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ title }}</ion-title>
+          <ion-buttons slot="end">
+            <ion-button @click="openModal">
+              <ion-icon aria-hidden="true" :icon="settingsSharp"></ion-icon>
+            </ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>
       <slot></slot>
     </ion-content>
   </ion-page>
 </template>
+
+<style scoped lang="scss">
+  ion-toolbar {
+    --background: var(--ion-color-primary);
+    padding: 0 0 20px 0;
+  }
+  ion-title {
+    color: white;
+    margin: 10px;
+  }
+  ion-icon {
+    color: white;
+    margin: 10px 10px 0 0;
+  }
+</style>
