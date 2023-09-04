e292adfeb8dc72ff2ef3c3cc50dd6950accf9f3d
Markus Kurbel <m4rku@pm.me>
2023-09-04 21:38:10 +0200

    Refactor Nutrition calendar

---
+++ src/layouts/PageWithNutritionCalendar.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+  import { IonGrid, IonRow } from '@ionic/vue';
+  import AppLayout from '@/layouts/AppLayout.vue';
+  import { ref, provide, readonly, reactive } from 'vue';
+  import { useDark } from '@vueuse/core'
+  import { selectedDateKey } from '@/keys';
+  import { useFoodDiaryStore } from '@/store/foodDiary';
+  import { useUserSettingsStore } from '@/store/userSettingsStore';
+  import { getCurrentWeekDates } from '@/helpers/time';
+
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    }
+  });
+
+  const userSettingsStore = useUserSettingsStore()
+  const setLocale = ref(userSettingsStore.getLocale())
+  const isDark = useDark()
+  const selectedDate = ref<Date>(new Date())
+  provide(selectedDateKey, readonly(selectedDate))
+
+  const nutritionStore = useFoodDiaryStore();
+
+  let nutritionEventDates = reactive<Date[]>([])
+  nutritionEventDates = getCurrentWeekDates().filter(
+    date => nutritionStore.getFoodDiaryEntries.some(foodEntry => new Date(foodEntry.createdAt).getDay() === date.getDay()))
+
+  const attributes = ref([{
+    key: 'today',
+    highlight: {
+      color: '#3F63C8',
+      fillMode: 'light',
+    },
+    dates: new Date(),
+  },
+  {
+    key: 'nutritionEvent',
+    dot: {
+      style: {
+        backgroundColor: '#3F63C8',
+      },
+    },
+    dates: nutritionEventDates,
+  }
+  ])
+
+  nutritionStore.$subscribe((mutation, state) => {
+    console.log('mutation', mutation);
+    nutritionEventDates = getCurrentWeekDates().filter(
+      date => nutritionStore.getFoodDiaryEntries.some(foodEntry => new Date(foodEntry.createdAt).getDay() === date.getDay()))
+  });
+</script>
+
+<template>
+  <AppLayout :title=title>
+    <ion-grid>
+      <ion-row class="ion-justify-content-center">
+        <ion-card class="ion-margin">
+          <VDatePicker 
+            class="vc-arrow-icon"
+            :locale=setLocale
+            title-position="left"
+            expanded 
+            transparent 
+            borderless 
+            :is-dark="isDark" 
+            view="weekly"
+            v-model="selectedDate"
+            :attributes="attributes"
+          />
+        </ion-card>
+      </ion-row>
+    </ion-grid>
+    <slot></slot>
+  </AppLayout>
+</template>
+
+<style scoped lang="scss">
+  ion-card {
+    width: 100%;
+  }
+</style>
--- src/views/Nutrition.vue
+++ src/views/Nutrition.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-  import { IonGrid, IonRow, IonSegment, IonSegmentButton, IonLabel } from '@ionic/vue';
-  import AppLayout from '@/layouts/AppLayout.vue';
-  import { ref, provide, readonly, reactive } from 'vue';
-  import { useDark } from '@vueuse/core'
-  import { selectedDateKey } from '@/keys';
-  import { useFoodDiaryStore } from '@/store/foodDiary';
-  import { getCurrentWeekDates } from '@/helpers/time';
+  import PageWithNutritionCalendar from '@/layouts/PageWithNutritionCalendar.vue';
+  import { ref } from 'vue';
 
   import BaseSegments from '@/components/BaseSegments.vue';
   import NutritionFoodDiaryContent from '@/components/NutritionFoodDiaryContent.vue';
   import NutritionYourDayContent from '@/components/NutritionYourDayContent.vue';
 
-  const selectedSegment = ref('day');
   const diarySelected = ref(false)
   const yourDaySelected = ref(true)
 
@@ -24,58 +18,10 @@
      diarySelected.value = false;
      yourDaySelected.value = true;
   }
-
-  const isDark = useDark()
-  const selectedDate = ref<Date>(new Date())
-  provide(selectedDateKey, readonly(selectedDate))
-
-  const nutritionStore = useFoodDiaryStore();
-  let nutritionEventDates = reactive<Date[]>([])
-  nutritionEventDates = getCurrentWeekDates().filter(
-    date => nutritionStore.getFoodDiaryEntries.some(foodEntry => new Date(foodEntry.createdAt).getDay() === date.getDay()))
-
-  console.log(nutritionEventDates)
-  const attributes = ref([{
-    key: 'today',
-    highlight: {
-      color: '#3F63C8',
-      fillMode: 'light',
-    },
-    dates: new Date(),
-  },
-  {
-    key: 'nutritionEvent',
-    dot: {
-      style: {
-        backgroundColor: '#3F63C8',
-      },
-    },
-    dates: nutritionEventDates,
-  }
-  ])
-
-  nutritionStore.$subscribe((mutation, state) => {
-    console.log('mutation', mutation);
-    nutritionEventDates = getCurrentWeekDates().filter(
-      date => nutritionStore.getFoodDiaryEntries.some(foodEntry => new Date(foodEntry.createdAt).getDay() === date.getDay()))
-  });
 </script>
 
 <template>
-  <AppLayout :title="$t('nutrition.title')">
-    <ion-grid>
-      <ion-row class="ion-justify-content-center">
-          <VDatePicker 
-            expanded 
-            transparent 
-            borderless 
-            :is-dark="isDark" 
-            view="weekly"
-            v-model="selectedDate"
-            :attributes="attributes"
-          />
-      </ion-row>
-    </ion-grid>
+  <PageWithNutritionCalendar :title="$t('nutrition.title')">
     <BaseSegments
       :leftSegmentLabel="$t('nutrition.yourDay')"
       :rightSegmentLabel="$t('nutrition.foodDiary')"
@@ -84,5 +30,5 @@
     />
     <NutritionFoodDiaryContent v-if="diarySelected" />
     <NutritionYourDayContent v-if="yourDaySelected" />
-  </AppLayout>
+  </PageWithNutritionCalendar>
 </template>
