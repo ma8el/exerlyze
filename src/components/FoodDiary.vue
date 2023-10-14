1e78634d4e0c12615168036125683e56ba42d0cd
Markus Kurbel <m4rku@pm.me>
2023-10-14 10:22:19 +0200

    Style food diary

--- src/components/FoodDiary.vue
+++ src/components/FoodDiary.vue
@@ -3,61 +3,76 @@
            IonItemGroup,
            IonItemDivider,
            IonLabel,
-           IonCard } from '@ionic/vue';
+           IonIcon } from '@ionic/vue';
   import { useFoodDiaryStore } from '@/store/foodDiary';
   import FoodItem from './FoodItem.vue';
   import { inject } from 'vue';
   import { selectedDateKey } from '@/keys';
+import { chevronDownOutline } from 'ionicons/icons';
 
   const selectedDate = inject(selectedDateKey)
   const foodDiaryStore = useFoodDiaryStore();
 </script>
 
 <template>
-  <ion-card>
-    <ion-list>
-      <ion-item-group>
-        <ion-item-divider>
-          <ion-label>
-            {{ $t('nutrition.breakfast') }}
-          </ion-label>
-        </ion-item-divider>
-        <FoodItem 
-          v-for="(foodItem, index) in foodDiaryStore.getBreakfastEntriesOfDate(selectedDate)"
-          v-if="selectedDate"
-          :key="foodItem.id"
-          :foodName="foodItem.foodName"
-          :calories="foodItem.calories"
-        />
-      </ion-item-group>
-      <ion-item-group>
-        <ion-item-divider>
-          <ion-label>
-            {{ $t('nutrition.lunch') }}
-          </ion-label>
-        </ion-item-divider>
-        <FoodItem 
-          v-for="(foodItem, index) in foodDiaryStore.getLunchEntriesOfDate(selectedDate)"
-          v-if="selectedDate"
-          :key="foodItem.id"
-          :foodName="foodItem.foodName"
-          :calories="foodItem.calories"
-        />
-      </ion-item-group>
-      <ion-item-group>
-        <ion-item-divider>
-          <ion-label>
-            {{ $t('nutrition.dinner') }}
-          </ion-label>
-        </ion-item-divider>
-        <FoodItem 
-          v-for="(foodItem, index) in foodDiaryStore.getDinnerEntriesOfDate(selectedDate)"
-          v-if="selectedDate"
-          :key="foodItem.id"
-          :foodName="foodItem.foodName"
-          :calories="foodItem.calories"
-        />
-      </ion-item-group>
-    </ion-list>
-  </ion-card>
+  <ion-list class="diary-list ion-padding">
+    <ion-item-group class="ion-padding-bottom">
+      <ion-item-divider>
+        <ion-label>
+          {{ $t('nutrition.breakfast') }}
+        </ion-label>
+        <ion-icon :icon="chevronDownOutline" slot="end"></ion-icon>
+      </ion-item-divider>
+      <FoodItem 
+        v-for="(foodItem, index) in foodDiaryStore.getBreakfastEntriesOfDate(selectedDate)"
+        v-if="selectedDate"
+        :key="foodItem.id"
+        :foodName="foodItem.foodName"
+        :calories="foodItem.calories"
+        :quantity="foodItem.quantity"
+      />
+    </ion-item-group>
+    <ion-item-group class="ion-padding-bottom">
+      <ion-item-divider>
+        <ion-label>
+          {{ $t('nutrition.lunch') }}
+        </ion-label>
+        <ion-icon :icon="chevronDownOutline" slot="end"></ion-icon>
+      </ion-item-divider>
+      <FoodItem 
+        v-for="(foodItem, index) in foodDiaryStore.getLunchEntriesOfDate(selectedDate)"
+        v-if="selectedDate"
+        :key="foodItem.id"
+        :foodName="foodItem.foodName"
+        :calories="foodItem.calories"
+        :quantity="foodItem.quantity"
+      />
+    </ion-item-group>
+    <ion-item-group class="ion-padding-bottom">
+      <ion-item-divider>
+        <ion-label>
+          {{ $t('nutrition.dinner') }}
+        </ion-label>
+        <ion-icon :icon="chevronDownOutline" slot="end"></ion-icon>
+      </ion-item-divider>
+      <FoodItem 
+        v-for="(foodItem, index) in foodDiaryStore.getDinnerEntriesOfDate(selectedDate)"
+        v-if="selectedDate"
+        :key="foodItem.id"
+        :foodName="foodItem.foodName"
+        :calories="foodItem.calories"
+        :quantity="foodItem.quantity"
+      />
+    </ion-item-group>
+  </ion-list>
 </template>
+
+<style scoped>
+ion-list {
+  background: none;
+  margin-bottom: 100px;
+}
+ion-item-group {
+  border-radius: 50px;
+}
+</style>
--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -13,17 +13,43 @@
     foodImage: {
       type: String,
       default: undefined
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    unit: {
+      type: String,
+      default: 'g'
     }
   })
-
 </script>
 
 <template>
   <ion-item>
-    <ion-avatar v-if="foodImage" slot="start">
-      <img :src="foodImage" />
-    </ion-avatar>
-    <ion-label>{{ foodName }}</ion-label>
+    <img :src="foodImage" class="product-img" />
+    <ion-label>
+      <p>{{ foodName }}</p>
+      <p>{{ quantity }} {{ unit }}</p>
+    </ion-label>
     <ion-note slot="end">{{ calories }} kcal</ion-note>
   </ion-item>
 </template>
+
+<style scoped>
+.product-img {
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+}
+ion-item {
+  border-radius: 10px;
+  margin-bottom: 10px;
+  p {
+    color: white;
+  };
+  ion-note {
+    color: white;
+  }
+}
+</style>
