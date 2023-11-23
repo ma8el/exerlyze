072d309a90b13e1e65d325c0aab5d0578b3015c9
Markus Kurbel <m4rku@pm.me>
2023-11-23 17:28:58 +0100

    Add network listener

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,15 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { Network } from '@capacitor/network';
+
+onMounted(async () => {
+  Network.addListener('networkStatusChange', status => {
+    console.log('Network status changed', status);
+  });
+  const status = await Network.getStatus();
+  console.log('Network status:', status);
+});
 </script>
