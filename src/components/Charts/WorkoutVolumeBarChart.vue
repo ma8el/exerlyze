f6605a71bef62469b7313578cfb6d144694f65fe
Markus Kurbel <m4rku@pm.me>
2023-11-25 14:28:35 +0100

    Fix volume chart reactivity

--- src/components/Charts/WorkoutVolumeBarChart.vue
+++ src/components/Charts/WorkoutVolumeBarChart.vue
@@ -3,13 +3,13 @@ import { use } from 'echarts/core'
 import { LineChart } from 'echarts/charts'
 import { TitleComponent, LegendComponent, GridComponent } from 'echarts/components'
 import { CanvasRenderer } from 'echarts/renderers'
-import VChart, { THEME_KEY } from 'vue-echarts';
+import VChart from 'vue-echarts';
 
 import { useI18n } from 'vue-i18n';
 import { useDayOfWeekStore, useWorkoutSessionStore } from '@/store/workoutStore';
 import { getCurrentWeekDates } from '@/helpers/time';
 import BaseChartContainer from './BaseChartContainer.vue';
-import { computed } from 'vue';
+import { computed, reactive, onMounted } from 'vue';
 
 use([TitleComponent, LegendComponent, GridComponent, LineChart, CanvasRenderer])
 
@@ -18,19 +18,22 @@ const { t } = useI18n();
 const dayOfWeekStore = useDayOfWeekStore();
 const weekDays = getCurrentWeekDates();
 const workoutSessionStore = useWorkoutSessionStore();
-const weeklyWorkoutVolume = weekDays.map(day => {
+const weeklyWorkoutVolume = computed(() => {
+  return weekDays.map(day => {
   const workoutSessions = workoutSessionStore.getWorkoutSessionPerformanceByDate(day)
   return workoutSessions
   ? workoutSessions.reduce(
     (acc, curr) => acc + curr.performed_reps * curr.performed_weight, 0)
   : 0
+  })
 });
 
 const hasData = computed (() => {
-  return weeklyWorkoutVolume.reduce((acc, curr) => acc + curr, 0) > 0;
+  return weeklyWorkoutVolume.value.reduce((acc, curr) => acc + curr, 0) > 0;
 });
 
-const option = {
+const getOptions = () => {
+ return {
   title: {
     text: t('home.workoutVolume'),
     textStyle: {
@@ -69,15 +72,25 @@ const option = {
   series: [
     {
       name: t('home.workoutVolume'),
-      data: weeklyWorkoutVolume,
+      data: weeklyWorkoutVolume.value,
       type: 'line',
       areaStyle: {},
       smooth: true,
       color: '#3F63C8',
     }
   ]
+ }
 };
 
+let option = reactive(getOptions());
+
+workoutSessionStore.$subscribe((mutation, state) => {
+  Object.assign(option, getOptions());
+});
+
+onMounted(() => {
+  Object.assign(option, getOptions());
+});
 </script>
 
 <template>
