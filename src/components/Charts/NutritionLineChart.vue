7461e92972926eb1f28a4b2df04cb63c55d74ca9
Markus Kurbel <m4rku@pm.me>
2023-11-25 14:20:16 +0100

    Add nutrition chart reactivity

--- src/components/Charts/NutritionLineChart.vue
+++ src/components/Charts/NutritionLineChart.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { use } from 'echarts/core'
 import { LineChart } from 'echarts/charts'
-import { TitleComponent, TooltipComponent, LegendComponent, GridComponent, VisualMapComponent, MarkLineComponent } from 'echarts/components'
+import { TitleComponent, TooltipComponent, GridComponent, VisualMapComponent, MarkLineComponent } from 'echarts/components'
 import { CanvasRenderer } from 'echarts/renderers'
-import VChart, { THEME_KEY } from 'vue-echarts';
+import VChart from 'vue-echarts';
 
 import { useI18n } from 'vue-i18n';
 import { useDayOfWeekStore } from '@/store/workoutStore';
 import { useFoodDiaryStore } from '@/store/foodDiary';
 import { getCurrentWeekDates } from '@/helpers/time';
 import BaseChartContainer from './BaseChartContainer.vue';
-import { computed } from 'vue';
+import { computed, onMounted, reactive } from 'vue';
 
 use([TitleComponent, TooltipComponent, GridComponent, VisualMapComponent, MarkLineComponent, LineChart, CanvasRenderer])
 
@@ -20,93 +20,107 @@ const dayOfWeekStore = useDayOfWeekStore();
 const weekDays = getCurrentWeekDates();
 const foodDiaryStore = useFoodDiaryStore();
 
-const weeklyConsumedCalories = weekDays.map(day => {
+const weeklyConsumedCalories = computed(() => { 
+  return weekDays.map(day => {
   const consumedCalories = foodDiaryStore.getCaloriesOfDate(day)
   return consumedCalories
+  })
 });
 
 const hasData = computed (() => {
-  return weeklyConsumedCalories.reduce((acc, curr) => acc + curr, 0) > 0;
+  return weeklyConsumedCalories.value.reduce((acc, curr) => acc + curr, 0) > 0;
 });
 
-const option = {
-  title: {
-    text: t('home.consumedCalories'),
-    textStyle: {
-      color: 'white',
-    },
-  },
-  legend: {
-    data: [t('home.consumedCalories')],
-    textStyle: {
-      color: 'white',
-    },
-    right: 10,
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: dayOfWeekStore.daysOfWeek.map(d => d.name.slice(0, 3)),
-  },
-  yAxis: {
-    type: 'value',
-    axisLine: {
-      lineStyle: {
-        color: 'grey',
+const getOptions = () => {
+  return {
+    title: {
+      text: t('home.consumedCalories'),
+      textStyle: {
+        color: 'white',
       },
     },
-    splitLine: {
-      lineStyle: {
-        color: 'grey', // set the color of the x-axis grid
+    legend: {
+      data: [t('home.consumedCalories')],
+      textStyle: {
+        color: 'white',
       },
+      right: 10,
     },
-  },
-  visualMap: {
-    show: false,
-    pieces: [
-      {
-        gt: 0,
-        lte: foodDiaryStore.dailyCalories,
-        color: '#8BC34A'
-      },
-      {
-        gt: foodDiaryStore.dailyCalories,
-        color: '#FC4D55'
-      }
-    ],
-    outOfRange: {
-      color: '#999'
-    }
-  },
-  grid: {
-    left: 40,
-    bottom: 20,
-  },
-  series: [
-    {
-      name: t('home.consumedCalories'),
-      data: weeklyConsumedCalories,
-      type: 'line',
-      areaStyle: {},
-      smooth: true,
-      color: '#93CE07',
-      markLine: {
-        silent: true,
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: dayOfWeekStore.daysOfWeek.map(d => d.name.slice(0, 3)),
+    },
+    yAxis: {
+      type: 'value',
+      axisLine: {
         lineStyle: {
-          color: '#FFFFFF'
+          color: 'grey',
         },
-        label: {
-          color: '#FFFFFF'
+      },
+      splitLine: {
+        lineStyle: {
+          color: 'grey', // set the color of the x-axis grid
         },
-        data: [
-          {
-            yAxis: foodDiaryStore.dailyCalories
-          }
-        ]
+      },
+    },
+    visualMap: {
+      show: false,
+      pieces: [
+        {
+          gt: 0,
+          lte: foodDiaryStore.dailyCalories,
+          color: '#8BC34A'
+        },
+        {
+          gt: foodDiaryStore.dailyCalories,
+          color: '#FC4D55'
+        }
+      ],
+      outOfRange: {
+        color: '#999'
       }
     },
-  ]
+    grid: {
+      left: 40,
+      bottom: 20,
+    },
+    series: [
+      {
+        name: t('home.consumedCalories'),
+        data: weeklyConsumedCalories.value,
+        type: 'line',
+        areaStyle: {},
+        smooth: true,
+        color: '#93CE07',
+        markLine: {
+          silent: true,
+          lineStyle: {
+            color: '#FFFFFF'
+          },
+          label: {
+            color: '#FFFFFF'
+          },
+          data: [
+            {
+              yAxis: foodDiaryStore.dailyCalories
+            }
+          ]
+        }
+      },
+    ]
+  }
 };
+
+let option = reactive(getOptions());
+
+foodDiaryStore.$subscribe((mutation, state) => {
+  Object.assign(option, getOptions());
+});
+
+onMounted(() => {
+  Object.assign(option, getOptions());
+});
 </script>
 
 <template>
