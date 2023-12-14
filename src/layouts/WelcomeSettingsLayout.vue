070d73312e7bb7ea316ce6139807bf31f927ec5a
Markus Kurbel <m4rku@pm.me>
2023-12-14 14:45:36 +0100

    Add layout for initial setup

---
+++ src/layouts/WelcomeSettingsLayout.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { IonContent, IonLabel } from '@ionic/vue'
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <ion-content class="ion-padding">
+    <div class="image">
+      <img :src="image" alt="">
+    </div>
+    <div class="name">
+      <ion-label class="bold_lbl">{{ title }}</ion-label>
+      <ion-label class="smpl_lbl">{{ subtitle }}</ion-label>
+    </div>
+    <slot></slot>
+ </ion-content>
+</template>
+
+<style scoped lang="scss">
+ion-content{
+    .image{
+        margin-top: 2rem;
+        display: flex;
+        justify-content: center;
+        img{
+            height: 300px;
+            width: 300px;
+        }
+    }
+    .name{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        margin-top: 1.5rem;
+        .bold_lbl{
+            font-size: 24px;
+            margin-bottom: 10px;
+            span{
+                font-size: 44px;
+                color: var(--ion-color-primary);
+            }
+        }
+        .smpl_lbl{
+            font-size: 14px;
+            margin-bottom: 16px;
+        }
+    }
+    :is(ion-item) {
+        border: 1px solid #92949c2a;
+        border-radius: 10px;
+        margin-top: 16px;
+        :is(ion-input) {
+            --padding-start: 10px;
+        }
+    }
+    .button{
+        margin-top: 1.5rem;
+        :is(ion-button){
+            height: 50px;
+        }
+    }
+}
+</style>
--- src/views/CompleteProfile.vue
+++ src/views/CompleteProfile.vue
@@ -1,67 +1,16 @@
 <script setup lang="ts">
-import { IonContent, IonLabel } from '@ionic/vue'
+import WelcomeSettingsLayout from '@/layouts/WelcomeSettingsLayout.vue';
 import UserMetricsInput from '@/components/UserMetricsInput.vue';
 </script>
 
 <template>
-  <ion-content class="ion-padding">
-    <div class="image">
-      <img src="../../assets/illustrations/fill_form.svg" alt="">
-    </div>
-    <div class="name">
-      <ion-label class="bold_lbl">{{ $t('completeProfile') }}</ion-label>
-      <ion-label class="smpl_lbl">{{ $t('completeProfileMessage') }}</ion-label>
-    </div>
-  
-    <UserMetricsInput
-      :buttonLabel="$t('next')"
-    />
-  </ion-content>
+    <WelcomeSettingsLayout
+      image="../../assets/illustrations/fill_form.svg"
+      :title="$t('completeProfile')"
+      :subtitle="$t('completeProfileMessage')"
+    >
+      <UserMetricsInput
+        :buttonLabel="$t('next')"
+      />
+    </WelcomeSettingsLayout>
 </template>
-
-<style scoped lang="scss">
-ion-content{
-    .image{
-        margin-top: 2rem;
-        display: flex;
-        justify-content: center;
-        img{
-            height: 300px;
-            width: 300px;
-        }
-    }
-    .name{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        margin-top: 1.5rem;
-        .bold_lbl{
-            font-size: 24px;
-            margin-bottom: 10px;
-            span{
-                font-size: 44px;
-                color: var(--ion-color-primary);
-            }
-        }
-        .smpl_lbl{
-            font-size: 14px;
-            margin-bottom: 16px;
-        }
-    }
-    :is(ion-item) {
-        border: 1px solid #92949c2a;
-        border-radius: 10px;
-        margin-top: 16px;
-        :is(ion-input) {
-            --padding-start: 10px;
-        }
-    }
-    .button{
-        margin-top: 1.5rem;
-        :is(ion-button){
-            height: 50px;
-        }
-    }
-}
-</style>
