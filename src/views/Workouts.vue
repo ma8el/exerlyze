0a36750168b816b8840b1d68223439786b48c64e
Markus Kurbel <m4rku@pm.me>
2023-12-22 16:45:32 +0100

    Remove workout plan components

--- src/views/Workouts.vue
+++ src/views/Workouts.vue
@@ -1,38 +1,10 @@
 <script setup lang="ts">
   import PageWithCalendar from '@/layouts/PageWithCalendar.vue';
-  import { ref } from 'vue';
   import WorkoutsContent from '@/components/WorkoutsContent.vue';
-  import WorkoutPlansContent from '@/components/WorkoutPlansContent.vue';
-  import BaseSegments from '@/components/BaseSegments.vue';
-
-  const workoutsSelected = ref(true)
-  const plansSelected = ref(false)
-  const scheduleSelected = ref(false)
-
-  const selectWorkouts = () => {
-     workoutsSelected.value = true;
-     plansSelected.value = false;
-     scheduleSelected.value = false;
-  }
-
-  const selectPlans = () => {
-     workoutsSelected.value = false;
-     plansSelected.value = true;
-     scheduleSelected.value = false;
-  }
-
 </script>
 
 <template>
   <PageWithCalendar :title="$t('workouts.title')">
-    
-    <BaseSegments
-      :leftSegmentLabel="$t('workouts.workout')"
-      :rightSegmentLabel="$t('workouts.plan', 2)"
-      @leftSegmentSelected="selectWorkouts"
-      @rightSegmentSelected="selectPlans"
-    />
-    <WorkoutsContent v-if="workoutsSelected" />
-    <WorkoutPlansContent v-if="plansSelected" />
+    <WorkoutsContent />
   </PageWithCalendar>
 </template>
