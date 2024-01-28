1f4344079c3cc7180b614dd354183a330eb41db2
Markus Kurbel <m4rku@pm.me>
2024-01-28 20:52:51 +0100

    Restrict app size

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
 
 <template>
-  <ion-app>
+  <ion-app class="app-container">
     <ion-router-outlet />
- </ion-app>
+  </ion-app>
 </template>
+
+<style scoped>
+@media screen and (min-width: 600px){
+  .app-container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+}
+</style>
